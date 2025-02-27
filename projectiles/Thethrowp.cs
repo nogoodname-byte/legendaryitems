af0fa5da8a73798f676af81f2638eed57b9410ae
nogoodname-byte <sethvt2007@icloud.com>
2025-02-26 18:27:36 -0800

    Add files via upload

--- projectiles/Thethrowp.cs
+++ projectiles/Thethrowp.cs
@@ -12,27 +12,12 @@ namespace legendaryitems.projectiles
 {
     public class Thethrowp : ModProjectile
     {
-        int sec = 0;
-        int thehit = 0;
-        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
-        {
-            target.AddBuff(BuffID.Frostburn2, 60);
-            //base.OnHitNPC(target, hit, ++thehit);
-            if (thehit < 10)
-            {
-                target.AddBuff(BuffID.WeaponImbueCursedFlames, 60);
-                ++thehit;
-            }
-        }
+        public int sec = 0;
         public override void SetStaticDefaults()
         {
             if (sec < 2)
             {
                 sec = 2;
-            }
-            if (thehit > 0)
-            {
-                sec = 2 + thehit;
             }
             ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 320;
             ProjectileID.Sets.YoyosLifeTimeMultiplier[Projectile.type] = sec;
