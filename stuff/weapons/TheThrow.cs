5b14b59aa1ea89bd89b94990f9e5478df9b611ac
bird guy.cs <120428688+birdguy-cs@users.noreply.github.com>
2025-03-03 16:37:26 -0800

    Update TheThrow.cs

--- stuff/weapons/TheThrow.cs
+++ stuff/weapons/TheThrow.cs
@@ -10,7 +10,7 @@ namespace legendaryitems.stuff.weapons
 {
     public class TheThrow : ModItem    
     {
-        public static int sec;
+        public int sec;
         public override void SetStaticDefaults()
         {
             ItemID.Sets.Yoyo[Item.type] = true;                                                       // tells the code it is a Yoyo and to exicute Yoyo code
